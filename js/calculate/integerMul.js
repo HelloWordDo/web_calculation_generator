f790e7190b9092b5b12abbd01c5ef98dc66b8cec
liquancheng <quancheng.li@mobilityasia.com.cn>
2022-10-09 20:37:59 +0800

    fix 乘号样式

--- js/calculate/integerMul.js
+++ js/calculate/integerMul.js
@@ -3,7 +3,7 @@ function mul_1_1(issue) {
   i = rand(1, 9);
   j = rand(1, 9);
   issue.opr[0] = i;
-  issue.op[0] = 'x';
+  issue.op[0] = '×';
   issue.opr[1] = j;
   issue.result = issue.opr[0] * issue.opr[1];
 
